573bd5961ac196fe92c5202036a2eb55c08ed114
kcoffau <35386534+kcoffau@users.noreply.github.com>
2023-12-05 17:40:12 +1100

    Update __init__.py

--- custom_components/OVO_Australia/__init__.py
+++ custom_components/OVO_Australia/__init__.py
@@ -1,15 +1,5 @@
-# __init__.py
-from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN
-
-async def async_setup(hass, config):
-    # Your setup logic, if any
-    return True
-
-async def async_setup_entry(hass, entry):
-    # Your setup entry logic, if any
-    return True
-
-async def async_remove_entry(hass, entry):
-    # Your remove entry logic, if any
+DOMAIN = 'ovoenergyau'
+def async_setup(hass, config):
+    """Set up the OVO Energy Australia component."""
+    hass.data[DOMAIN] = {}
     return True
